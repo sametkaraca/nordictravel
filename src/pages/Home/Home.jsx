7ef48491e11848981b02dd8648a8b4e37ac10317
Samet <samet.karaca.no@gmail.com>
2020-11-13 12:52:54 +0100

    Boiler palte is ready.

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import style from "../../styles/_home.module.scss";
+
+export const Home = () => {
+  return <section className={style.home}>Home</section>;
+};
---
+++ src/pages/Pages.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+// import { About } from "./About/About";
+import { Home } from "./Home/Home";
+// import { Project } from "./Project/Project";
+// import { Error } from "./Error";
+// import { Lab } from "./Lab/Lab";
+
+export const Pages = () => {
+  return <Home />;
+  //   return (
+  //     <Switch>
+  //       <Route path="/About" component={About} exact />
+  //       <Route path="/Projects" component={Project} exact />
+  //       <Route path="/Lab" component={Lab} exact />
+  //       <Route path="/Home" component={Home} exact />
+  //       <Route path="/" component={Home} exact />
+  //       <Route component={Error} exact />
+  //     </Switch>
+  //   );
+};

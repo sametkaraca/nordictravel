1e5eda6d77b38b7cbb52ff48dd5dc5f7f1789820
Samet <samet.karaca.no@gmail.com>
2020-11-16 11:26:58 +0100

    Social media icons are added. Font sizes are changed.

--- src/pages/Home/HomeCountry.jsx
+++ src/pages/Home/HomeCountry.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import style from "../../styles/_home.module.scss";
 import playIcon from "../../assets/play.svg";
 
+import twitterIcon from "../../assets/twitter.svg";
+import facebookIcon from "../../assets/facebook.svg";
+
 export const HomeCountry = ({ image }) => {
   return (
     <section className={style.home_country}>
@@ -15,6 +18,7 @@ export const HomeCountry = ({ image }) => {
           className={style.home_country__picture__img}
         />
       </picture>
+
       <div className={style.home_country_container}>
         <h1 className={style.home_country_container__title}>
           Best of <br />
@@ -43,6 +47,19 @@ export const HomeCountry = ({ image }) => {
         </a>
 
         <span className={style.home_country_container__watch}>Watch Real</span>
+
+        <div className={style.home_country_container_icons}>
+          <img
+            className={style.home_country_container_icons__img}
+            src={twitterIcon}
+            alt={`Twitter`}
+          />
+          <img
+            className={style.home_country_container_icons__img}
+            src={facebookIcon}
+            alt={`Facebook`}
+          />
+        </div>
       </div>
     </section>
   );
